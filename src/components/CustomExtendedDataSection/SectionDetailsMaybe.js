nouakil63/web-template

Refactor extended data fields to use shared components
Earlier, ProfilePage and ListingPage variants used separate
components for displaying extended data. This change separates
the user and listing specific logic from the display logic and
moves the display logic to shared components. In addition,
ListingPage variants now use a shared SectionTextMaybe for showing
the listing description.

src/components/CustomExtendedDataSection/SectionDetailsMaybe.js
@@ -1,0 +1,37 @@
+import React from 'react';
+
+import { FormattedMessage } from '../../util/reactIntl';
+
+import { Heading } from '../../components';
+
+import css from './CustomExtendedDataSection.module.css';
+
+const SectionDetailsMaybe = props => {
+  const { fieldConfigs, page, pickExtendedDataFields } = props;
+
+  if (!fieldConfigs) {
+    return null;
+  }
+
+  const existingFields = fieldConfigs?.reduce(pickExtendedDataFields, []);
+
+  const heading = `${page}.detailsTitle`;
+
+  return existingFields?.length > 0 ? (
+    <section className={css.sectionDetails}>
+      <Heading as="h2" rootClassName={css.sectionHeading}>
+        <FormattedMessage id={heading} />
+      </Heading>
+      <ul className={css.details}>
+        {existingFields.map(detail => (
+          <li key={detail.key} className={css.detailsRow}>
+            <span className={css.detailLabel}>{detail.label}</span>
+            <span>{detail.value}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  ) : null;
+};
+
+export default SectionDetailsMaybe;

src/components/CustomExtendedDataSection/SectionMultiEnumMaybe.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Heading, PropertyGroup } from '../../components';
 
-import css from './ProfilePage.module.css';
+import css from './CustomExtendedDataSection.module.css';
 
 const SectionMultiEnumMaybe = props => {
-  const { heading, options, selectedOptions, showUnselectedOptions = true } = props;
+  const { heading, options, selectedOptions, page, showUnselectedOptions = true } = props;
   const hasContent = showUnselectedOptions || selectedOptions?.length > 0;
   if (!heading || !options || !hasContent) {
     return null;
@@ -12,19 +12,19 @@ const SectionMultiEnumMaybe = props => {
   const idSlug = heading.toLowerCase().replace(/ /g, '_');
 
   return (
-    <div className={css.sectionMultiEnum}>
+    <section className={css.sectionMultiEnum}>
       <Heading as="h2" rootClassName={css.sectionHeading}>
         {heading}
       </Heading>
       <PropertyGroup
-        id={`ProfilePage.${idSlug}`}
+        id={`${page}.${idSlug}`}
         ariaLabel={heading}
         options={options}
         selectedOptions={selectedOptions}
         twoColumns={options.length > 5}
         showUnselectedOptions={showUnselectedOptions}
       />
-    </div>
+    </section>
   );
 };
 

Oluwanifemiiii/rewaebiclosetdev

src/components/CustomExtendedDataSection/SectionDetailsMaybe.js
@@ -1,0 +1,35 @@
+import React from 'react';
+
+import { FormattedMessage } from '../../util/reactIntl';
+
+import { Heading } from '../../components';
+
+import css from './CustomExtendedDataSection.module.css';
+
+const SectionDetailsMaybe = props => {
+  const { fieldConfigs, pickExtendedDataFields, heading } = props;
+
+  if (!fieldConfigs) {
+    return null;
+  }
+
+  const existingFields = fieldConfigs?.reduce(pickExtendedDataFields, []);
+
+  return existingFields?.length > 0 ? (
+    <section className={css.sectionDetails}>
+      <Heading as="h2" rootClassName={css.sectionHeading}>
+        <FormattedMessage id={heading} />
+      </Heading>
+      <ul className={css.details}>
+        {existingFields.map(detail => (
+          <li key={detail.key} className={css.detailsRow}>
+            <span className={css.detailLabel}>{detail.label}</span>
+            <span>{detail.value}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  ) : null;
+};
+
+export default SectionDetailsMaybe;

src/components/CustomExtendedDataSection/SectionMultiEnumMaybe.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Heading, PropertyGroup } from '../../components';
 
-import css from './ProfilePage.module.css';
+import css from './CustomExtendedDataSection.module.css';
 
 const SectionMultiEnumMaybe = props => {
-  const { heading, options, selectedOptions, showUnselectedOptions = true } = props;
+  const { heading, options, selectedOptions, idPrefix, showUnselectedOptions = true } = props;
   const hasContent = showUnselectedOptions || selectedOptions?.length > 0;
   if (!heading || !options || !hasContent) {
     return null;
@@ -12,19 +12,19 @@ const SectionMultiEnumMaybe = props => {
   const idSlug = heading.toLowerCase().replace(/ /g, '_');
 
   return (
-    <div className={css.sectionMultiEnum}>
+    <section className={css.sectionMultiEnum}>
       <Heading as="h2" rootClassName={css.sectionHeading}>
         {heading}
       </Heading>
       <PropertyGroup
-        id={`ProfilePage.${idSlug}`}
+        id={`${idPrefix}.${idSlug}`}
         ariaLabel={heading}
         options={options}
         selectedOptions={selectedOptions}
         twoColumns={options.length > 5}
         showUnselectedOptions={showUnselectedOptions}
       />
-    </div>
+    </section>
   );
 };
 