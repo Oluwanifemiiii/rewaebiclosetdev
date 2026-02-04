fe79f77dc059f8f942555e217b5bc0fb210e28aa
Oluwanifemiiii <93293777+Oluwanifemiiii@users.noreply.github.com>
2026-02-04 22:54:50 +0100

    removing cors duplicate

--- server/index.js
+++ server/index.js
@@ -45,7 +45,6 @@ const renderer = require('./renderer');
 const dataLoader = require('./dataLoader');
 const { generateCSPNonce, csp } = require('./csp');
 const sdkUtils = require('./api-util/sdk');
-const cors = require('cors');
 const buildPath = path.resolve(__dirname, '..', 'build');
 const dev = process.env.REACT_APP_ENV === 'development';
 const PORT = parseInt(process.env.PORT, 10);
