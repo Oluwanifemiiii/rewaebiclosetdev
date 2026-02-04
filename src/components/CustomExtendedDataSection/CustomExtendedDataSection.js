sharetribe/web-template

Rename CustomExtendedDataSection subcomponents

src/components/CustomExtendedDataSection/CustomExtendedDataSection.js
@@ -2,17 +2,34 @@ import React from 'react';
 
 import { SCHEMA_TYPE_MULTI_ENUM, SCHEMA_TYPE_TEXT, SCHEMA_TYPE_YOUTUBE } from '../../util/types';
 
-import SectionDetailsMaybe from './SectionDetailsMaybe';
-import SectionTextMaybe from './SectionTextMaybe';
-import SectionMultiEnumMaybe from './SectionMultiEnumMaybe';
-import SectionYoutubeVideoMaybe from './SectionYoutubeVideoMaybe';
+import SectionDetails from './SectionDetails';
+import SectionText from './SectionText';
+import SectionMultiEnum from './SectionMultiEnum';
+import SectionYoutubeVideo from './SectionYoutubeVideo';
 
+/**
+ * This component displays extended data that corresponds to asset based custom field
+ * configurations. It can be used for displaying listing, user, and transaction custom fields.
+ * - Fields with schema types enum, long, and boolean are displayed with a SectionDetails
+ *   subcomponent
+ * - Fields with schema type multi-enum, text, and youtubeVideoUrl are each displayed
+ *   with dedicated subcomponents
+ *
+ * @param {Object} props props for the component:
+ *   - sectionDetailsProps (Object) is passed directly to SectionDetails component
+ *   - propsForCustomFields (Array) can be constructed with the pickCustomFieldProps helper
+ *   - page is used to set translation key prefixes and component ids
+ *   - pickExtendedDataFields is an entity-specific function that determines what data
+ *     is selected to be picked from the entity's extended data
+ * @return A component that displays the existing extended data according to
+ * the provided field configuration
+ */
 const CustomExtendedDataSection = props => {
   const { sectionDetailsProps, propsForCustomFields = [], page, pickExtendedDataFields } = props;
 
   return (
     <>
-      <SectionDetailsMaybe
+      <SectionDetails
         {...sectionDetailsProps}
         page={page}
         pickExtendedDataFields={pickExtendedDataFields}
@@ -20,11 +37,11 @@ const CustomExtendedDataSection = props => {
       {propsForCustomFields.map(customFieldProps => {
         const { schemaType, key, ...fieldProps } = customFieldProps;
         return schemaType === SCHEMA_TYPE_MULTI_ENUM ? (
-          <SectionMultiEnumMaybe key={key} page={page} {...fieldProps} />
+          <SectionMultiEnum key={key} page={page} {...fieldProps} />
         ) : schemaType === SCHEMA_TYPE_TEXT ? (
-          <SectionTextMaybe key={key} {...fieldProps} />
+          <SectionText key={key} {...fieldProps} />
         ) : schemaType === SCHEMA_TYPE_YOUTUBE ? (
-          <SectionYoutubeVideoMaybe key={key} {...fieldProps} />
+          <SectionYoutubeVideo key={key} {...fieldProps} />
         ) : null;
       })}
     </>

Oluwanifemiiii/rewaebiclosetdev

src/components/CustomExtendedDataSection/CustomExtendedDataSection.js
@@ -2,11 +2,28 @@ import React from 'react';
 
 import { SCHEMA_TYPE_MULTI_ENUM, SCHEMA_TYPE_TEXT, SCHEMA_TYPE_YOUTUBE } from '../../util/types';
 
-import SectionDetailsMaybe from './SectionDetailsMaybe';
-import SectionTextMaybe from './SectionTextMaybe';
-import SectionMultiEnumMaybe from './SectionMultiEnumMaybe';
-import SectionYoutubeVideoMaybe from './SectionYoutubeVideoMaybe';
+import SectionDetails from './SectionDetails';
+import SectionText from './SectionText';
+import SectionMultiEnum from './SectionMultiEnum';
+import SectionYoutubeVideo from './SectionYoutubeVideo';
 
+/**
+ * This component displays extended data that corresponds to asset based custom field
+ * configurations. It can be used for displaying listing, user, and transaction custom fields.
+ * - Fields with schema types enum, long, and boolean are displayed with a SectionDetails
+ *   subcomponent
+ * - Fields with schema type multi-enum, text, and youtubeVideoUrl are each displayed
+ *   with dedicated subcomponents
+ *
+ * @param {Object} props props for the component:
+ *   - sectionDetailsProps (Object) is passed directly to SectionDetails component
+ *   - propsForCustomFields (Array) can be constructed with the pickCustomFieldProps helper
+ *   - page is used to set translation key prefixes and component ids
+ *   - pickExtendedDataFields is an entity-specific function that determines what data
+ *     is selected to be picked from the entity's extended data
+ * @return A component that displays the existing extended data according to
+ * the provided field configuration
+ */
 const CustomExtendedDataSection = props => {
   const {
     sectionDetailsProps,
@@ -17,18 +34,15 @@ const CustomExtendedDataSection = props => {
 
   return (
     <>
-      <SectionDetailsMaybe
-        {...sectionDetailsProps}
-        pickExtendedDataFields={pickExtendedDataFields}
-      />
+      <SectionDetails {...sectionDetailsProps} pickExtendedDataFields={pickExtendedDataFields} />
       {propsForCustomFields.map(customFieldProps => {
         const { schemaType, key, ...fieldProps } = customFieldProps;
         return schemaType === SCHEMA_TYPE_MULTI_ENUM ? (
-          <SectionMultiEnumMaybe key={key} idPrefix={idPrefix} {...fieldProps} />
+          <SectionMultiEnum key={key} idPrefix={idPrefix} {...fieldProps} />
         ) : schemaType === SCHEMA_TYPE_TEXT ? (
-          <SectionTextMaybe key={key} {...fieldProps} />
+          <SectionText key={key} {...fieldProps} />
         ) : schemaType === SCHEMA_TYPE_YOUTUBE ? (
-          <SectionYoutubeVideoMaybe key={key} {...fieldProps} />
+          <SectionYoutubeVideo key={key} {...fieldProps} />
         ) : null;
       })}
     </>